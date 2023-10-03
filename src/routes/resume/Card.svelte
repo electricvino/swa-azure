44a221e3e5abb0a03f00fda058b120f16a0b209d
Jon Vinson <jvinson@oneparkfinancial.com>
2023-10-03 14:35:05 -0400

    fix: update cards for resume ui

--- src/routes/resume/Card.svelte
+++ src/routes/resume/Card.svelte
@@ -1,35 +1,37 @@
 <script lang="ts">
-    export let image_src: string;
-    export let company: string;
-    export let job_title: string;
-    export let description: string[];
-    export let start: string;
-    export let end: string;
-    export let url: string;
-    export let tags: string[];
-</script>  
-  
+	export let image_src: string;
+	export let company: string;
+	export let job_title: string;
+	export let description: string[];
+	export let start: string;
+	export let end: string;
+	export let url: string;
+	export let tags: string[];
+</script>
+
 <a class="card card-hover overflow-hidden" href={url}>
-    <header>
-      <img src={image_src} alt="Post" class="bg-black/50 w-full aspect-[21/9]">
-    </header>
-    <div class="p-4 space-y-4">
-        <h2 class="h2">{company}</h2>        
-        <h3 class="h3">{job_title}</h3>
-        <h6 class="h6">{start} to {end}</h6>
-        <article>
-            <ul>
-                {#each description as line}
-                <li>{line}</li>
-                {/each}
-            </ul>
-        </article>
-    </div>
-    <hr class="opacity-50" />
-    <footer class="p-4 flex justify-start items-center space-x-4">
-        <div class="flex justify-center space-x-2">
-            {#each tags as tag}
-                <span class="chip variant-filled-surface">{tag}</span>
-            {/each}
-    </footer>
-</a> 
+	<header>
+		<img src={image_src} alt="Post" class="bg-black/50 w-full aspect-[21/9]" />
+	</header>
+	<div class="p-4 space-y-4">
+		<h2 class="h2">{company}</h2>
+		<h3 class="h3">{job_title}</h3>
+		<h6 class="h6">{start} to {end}</h6>
+		<article>
+			<ul>
+				{#each description as line}
+					<li>{line}</li>
+				{/each}
+			</ul>
+		</article>
+	</div>
+	<hr class="opacity-50" />
+	<footer class="p-4 flex justify-start items-center space-x-4">
+		<div class="flex flex-wrap justify-center leading-3 space-x-2">
+			{#each tags as tag}
+				<span class="chip variant-ghost-surface">{tag}</span>
+			{/each}
+		</div>
+	</footer>
+</a>
+
--- src/routes/resume/Deck.svelte
+++ src/routes/resume/Deck.svelte
@@ -1,12 +1,10 @@
 <script>
-    import jobs from '$lib/jobs.json';
-    import Card from './Card.svelte';
+	import jobs from '$lib/jobs.json';
+	import Card from './Card.svelte';
 </script>
 
-<div class="w-full text-token grid grid-cols-1 md:grid-cols-2 gap-4">
-
-  {#each jobs as job, i}
-    <Card {...job} />
-  {/each}
-
+<div class="w-full text-token grid grid-cols-1 md:grid-cols-4 gap-5">
+	{#each jobs as job, i}
+		<Card {...job} />
+	{/each}
 </div>
